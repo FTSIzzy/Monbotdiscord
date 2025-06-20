002ac418edbf48cfb3a7bd51622f9d62facafec7
Trashextasy <trashextasy2@gmail.com>
2025-06-20 15:54:11 +0200

    Ajout de nouvelles commandes fun (avatar, serveur, userinfo, dice)

--- bot/main.py
+++ bot/main.py
@@ -10,7 +10,7 @@ TOKEN = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
@@ -45,6 +45,41 @@ async def eightball(ctx, *, question):
 async def pileouface(ctx):
     resultat = random.choice(["Pile", "Face"])
     await ctx.send(f"🪙 {resultat} !")
+
+@bot.command()
+async def avatar(ctx, user: discord.Member = None):
+    user = user or ctx.author
+    await ctx.send(user.display_avatar.url)
+
+@bot.command()
+async def serveur(ctx):
+    guild = ctx.guild
+    embed = discord.Embed(
+        title=f"Infos du serveur {guild.name}",
+        color=discord.Color.blue()
+    )
+    embed.add_field(name="Membres", value=guild.member_count)
+    embed.add_field(name="Propriétaire", value=guild.owner)
+    embed.add_field(name="Créé le", value=guild.created_at.strftime("%d/%m/%Y"))
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def userinfo(ctx, user: discord.Member = None):
+    user = user or ctx.author
+    embed = discord.Embed(
+        title=f"Infos sur {user.display_name}",
+        color=user.color
+    )
+    embed.add_field(name="ID", value=user.id)
+    embed.add_field(name="Créé le", value=user.created_at.strftime("%d/%m/%Y"))
+    embed.add_field(name="A rejoint le", value=user.joined_at.strftime("%d/%m/%Y"))
+    embed.set_thumbnail(url=user.display_avatar.url)
+    await ctx.send(embed=embed)
+
+@bot.command()
+async def dice(ctx, faces: int = 6):
+    result = random.randint(1, faces)
+    await ctx.send(f"🎲 Tu as lancé un dé à {faces} faces et obtenu : **{result}**")
 
 # Load commands from the commands package
 for filename in os.listdir('./bot/commands'):
